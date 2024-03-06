de015adf3d5a489f7072b40c07cda36cff3e785f
李鹏军 <172942410@qq.com>
2024-03-06 15:16:25 +0800

    晚上打包流程和更新android ui依赖库

--- ncnn-android-yolov8/app/src/main/java/com/tencent/yolov8ncnn/MainActivity.java
+++ ncnn-android-yolov8/app/src/main/java/com/tencent/yolov8ncnn/MainActivity.java
@@ -1,16 +1,3 @@
-// Tencent is pleased to support the open source community by making ncnn available.
-//
-// Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
 
 package com.tencent.yolov8ncnn;
 
@@ -20,7 +7,6 @@ import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -29,8 +15,9 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.lianyun.perry.cback.YoloModel;
 
